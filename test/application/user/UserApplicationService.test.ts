23acba85ba5a81a2fc8e3ac16a7ade7b5d83e892
satodai <banana5388@gmail.com>
2020-08-16 00:23:45 +0900

    feature: Test preparation

---
+++ test/application/user/UserApplicationService.test.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+
+
+import UserApplicationService from '@/application/user/UserApplicationService';
+
+describe('Test User', () => {
+
+    it('test signup', () => {
+    })
+});

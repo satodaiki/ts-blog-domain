8fbd3e27ab42a8c81d7c52820aa407b79a6db853
satodai <banana5388@gmail.com>
2020-08-16 12:05:50 +0900

    feature: Introducing test tools and test code.

--- test/application/user/UserApplicationService.test.ts
+++ test/application/user/UserApplicationService.test.ts
@@ -1,10 +1,29 @@
 import * as chai from 'chai';
-
-
 import UserApplicationService from '@/application/user/UserApplicationService';
+import InMemoryUserRepository from '@/repositories/inMemory/InMemoryUserRepository';
+import UserData from '@/application/user/UserData';
 
 describe('Test User', () => {
 
-    it('test signup', () => {
-    })
+    const userApplicationService = new UserApplicationService(new InMemoryUserRepository());
+
+    it('signup', () => {
+        const name: string = "test"
+        const email: string = "test@test.com"
+        const password: string = "test"
+
+        const userData: UserData = userApplicationService.signUp(name, email, password);
+
+        chai.assert.strictEqual(userData.name, name);
+    });
+
+    it('login', () => {
+        const name: string = "test"
+        const email: string = "test@test.com"
+        const password: string = "test"
+
+        const userData: UserData = userApplicationService.signUp(name, email, password);
+
+        chai.expect(() => userApplicationService.login(email, password)).not.to.throw()
+    });
 });
